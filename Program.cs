f339ecbc68859f373334c85ba1e0eb6709494e59
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:41:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DivideByZero
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialising the variables.......
+            int numerator = 10;
+            int denominator = 0;
+
+            //Indivating the Exception.......
+            try
+            {
+                Console.WriteLine(numerator/denominator);
+            }
+            //Handling the exception......
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Can't divide by zero");
+            }
+        }
+    }
+}
